3d37f77ae0efacb281197b70b4e3159ed2ea44fd
Fabian Liske <19373177+FabianLiske@users.noreply.github.com>
2025-05-25 13:04:45 +0200

    btop hinzugefügt

--- install-conf.sh
+++ install-conf.sh
@@ -13,3 +13,8 @@ mkdir -p ~/.config/alacritty
 
 ln -sf ~/dotfiles/terminal/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -sf ~/dotfiles/terminal/catppuccin_macchiato.toml ~/.config/alacritty/catppuccin_macchiato.toml
+
+# btop
+mkdir -p ~/.config/btop
+
+ln -sf ~/dotfiles/btop/btop.conf ~/.config/btop/btop.conf
