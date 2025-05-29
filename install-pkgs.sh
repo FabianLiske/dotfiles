598fc63c76e5b329cc549e206b9f7b6cb2e6105c
Fabian Liske <19373177+FabianLiske@users.noreply.github.com>
2025-05-29 04:00:17 +0200

    Install Hyprland packages

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -1,8 +1,116 @@
 #!/bin/bash
+set -euo pipefail
 
-# -----------------------------------------------------
-# synchronizing package databases
-# -----------------------------------------------------
+##################
+### ROOT CHECK ###
+##################
+
+if [ "$(id -u)" = 0 ]; then
+    echo ":: This script shouldn't be run as root."
+    exit 1
+fi
+
+clear
+GREEN='\033[0;32m'
+NONE='\033[0m'
+
+#################
+### FUNCTIONS ###
+#################
+
+# Check if package is installed
+_isInstalledPacman() {
+    package="$1";
+    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")";
+    if [ -n "${check}" ] ; then
+        echo 0; #'0' means 'true' in Bash
+        return; #true
+    fi;
+    echo 1; #'1' means 'false' in Bash
+    return; #false
+}
+
+# Install required packages
+_installPackagesPacman() {
+    toInstall=();
+    for pkg; do
+        if [[ $(_isInstalledPacman "${pkg}") == 0 ]]; then
+            echo "${pkg} is already installed.";
+            continue;
+        fi;
+        toInstall+=("${pkg}");
+    done;
+    if [[ "${toInstall[@]}" == "" ]] ; then
+        # echo "All pacman packages are already installed.";
+        return;
+    fi;
+    printf "Package not installed:\n%s\n" "${toInstall[@]}";
+    sudo pacman --noconfirm -S "${toInstall[@]}";
+}
+
+#########################
+### REQUIRED PACKAGES ###
+#########################
+
+# Required packages for the installer
+installer_packages=(
+    "wget"
+    "unzip"
+    "gum"
+    "figlet"
+)
+
 sudo pacman -Sy
 echo
 
+echo ":: Ensuring required packages are installed..."
+_installPackagesPacman "${installer_packages[@]}";
+echo
+
+# make yay faster - do not use compression
+sudo sed -i "s/PKGEXT=.*/PKGEXT='.pkg.tar'/g" /etc/makepkg.conf
+sudo sed -i "s/SRCEXT=.*/SRCEXT='.src.tar'/g" /etc/makepkg.conf
+
+
+
+# Hyprland packages
+# TODO: separate out groups of packages into individual files
+
+echo -e "${GREEN}"
+figlet "Hyprland"
+echo -e "${NONE}"
+
+sudo pacman -Sy \
+    hyprland \
+    hyprland-qtutils \
+    hyprlock \
+    hypridle \
+    hyprpaper \
+    kitty \
+    libnotify \
+    mako \
+    qt5-wayland \
+    qt6-wayland \
+    sddm \
+    waybar \
+    wl-clipboard \
+    wofi \
+    xdg-desktop-portal-hyprland \
+    xorg-xwayland \
+    --noconfirm
+
+yay -S \
+    wlogout \
+    --noconfirm
+
+#############################
+### ENVIRONMENT VARIABLES ###
+#############################
+
+# https://wiki.hyprland.org/Nvidia/
+env = LIBVA_DRIVER_NAME,nvidia
+env = XDG_SESSION_TYPE,wayland
+env = GBM_BACKEND,nvidia-drm
+env = __GLX_VENDOR_LIBRARY_NAME,nvidia
+env = NVD_BACKEND,direct
+env = ELECTRON_OZONE_PLATFORM_HINT,auto
