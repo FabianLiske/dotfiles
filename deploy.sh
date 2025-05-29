687385598c70fd6c32a1983f01d01cd2b7e9e75c
Fabian Liske <19373177+FabianLiske@users.noreply.github.com>
2025-05-30 00:16:43 +0200

    typo fix

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 mkdir -p "$HOME/.config"
-mkdir -p "$HOME/.local.share"
+mkdir -p "$HOME/.local/share"
 mkdir -p "$HOME/.icons"
 
 cp -r "$HOME/dotfiles/configs/desktop/hypr" "$HOME/.config/"
@@ -14,8 +14,8 @@ cp -r "$HOME/dotfiles/configs/desktop/fonts" "$HOME/.local/share/"
 cp -r "$HOME/dotfiles/configs/monitoring/btop" "$HOME/.config/"
 cp -r "$HOME/dotfiles/configs/monitoring/fastfetch" "$HOME/.config/"
 cp -r "$HOME/dotfiles/configs/terminal/kitty" "$HOME/.config/"
-cp -r "$HOME/dotfiles/configs/shell/starship.toml" "$HOME/.config/"
 
+cp "$HOME/dotfiles/configs/shell/starship.toml" "$HOME/.config/"
 cp "$HOME/dotfiles/configs/shell/.bashrc" "$HOME/"
 cp "$HOME/dotfiles/configs/shell/.gitconfig" "$HOME/"
 
