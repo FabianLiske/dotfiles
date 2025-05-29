e684346f4c8b990b690c3e3bc4a222116e3b94ec
Fabian Liske <19373177+FabianLiske@users.noreply.github.com>
2025-05-30 00:22:37 +0200

    grrrrrrrr

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 mkdir -p "$HOME/.config"
-mkdir -p "$HOME/.local/share"
+mkdir -p "$HOME/.local/share/fonts"
 mkdir -p "$HOME/.icons"
 
 cp -r "$HOME/dotfiles/configs/desktop/hypr" "$HOME/.config/"
@@ -18,5 +18,7 @@ cp -r "$HOME/dotfiles/configs/terminal/kitty" "$HOME/.config/"
 cp "$HOME/dotfiles/configs/shell/starship.toml" "$HOME/.config/"
 cp "$HOME/dotfiles/configs/shell/.bashrc" "$HOME/"
 cp "$HOME/dotfiles/configs/shell/.gitconfig" "$HOME/"
+
+fc-cache -fv
 
 echo "Dotfiles copied!"
