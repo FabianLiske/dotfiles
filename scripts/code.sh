15de12264d6be101b92b720081677cbf6518cf3b
Fabian Liske <FabianLiske@users.noreply.github.com>
2025-05-30 19:00:08 +0200

    New Script for installing Code Extensions

--- scripts/code.sh
+++ scripts/code.sh
@@ -1,9 +1,49 @@
 #!/bin/bash
 set -euo pipefail
 
-# Download
-wget -O "/tmp/icons.vsix" "https://open-vsx.org/api/Catppuccin/catppuccin-vsc-icons/1.21.0/file/Catppuccin.catppuccin-vsc-icons-1.21.0.vsix"
-wget -O "/tmp/theme.vsix" "https://open-vsx.org/api/Catppuccin/catppuccin-vsc/3.17.0/file/Catppuccin.catppuccin-vsc-3.17.0.vsix"
+# Dependency Check
+for cmd in curl jq code figlet; do
+    command -v "$cmd" >/dev/null 2>&1 || {
+        echo "ERROR: '$cmd' not found. Please install it."; exit 1;
+    }
+done
 
-code --install-extension "/tmp/icons.vsix"
-code --install-extension "/tmp/theme.vsix"
+# Colors
+GREEN='\033[0;32m'
+NONE='\033[0m'
+
+# Banner
+echo -e "${GREEN}"
+figlet "Code Extensions"
+echo -e "${NONE}"
+
+# Extensions to install
+EXTENSIONS=(
+    "Catppuccin/catppuccin-vsc-icons"
+    "Catppuccin/catppuccin-vsc"
+)
+
+NUM_EXTENSIONS=${#EXTENSIONS[@]}
+COUNT=0
+
+for EXT in "${EXTENSIONS[@]}"; do
+    ((COUNT++))
+    echo "Installing Extension $COUNT/$NUM_EXTENSIONS: $EXT"
+    
+    # Get Extensions info
+    API_URL="https://open-vsx.org/api/${EXT}/latest"
+    JSON=$(curl -s "$API_URL")
+    
+    # Get download url for latest version
+    DOWNLOAD_URL=$(jq -r ".files.download" <<< "$JSON")
+    
+    # Replace / with - in Filename
+    SANITIZED_EXT="${EXT//\//-}"
+    
+    # Download and install extension
+    curl -L --fail -o "/tmp/$SANITIZED_EXT.vsix" "$DOWNLOAD_URL"
+    code --install-extension "/tmp/$SANITIZED_EXT.vsix" --force
+    
+done
+
+echo "✅ All extensions installed."
