7940cc45ee1e672a96daebb3e1ab55a64f32eab5
Fabian Liske <19373177+FabianLiske@users.noreply.github.com>
2025-06-01 19:50:22 +0200

    Added root check

--- spicetify.sh
+++ spicetify.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -euo pipefail
 
+##################
+### ROOT CHECK ###
+##################
+
+if [ "$(id -u)" = 0 ]; then
+    echo ":: This script shouldn't be run as root."
+    exit 1
+fi
+
+clear
+GREEN='\033[0;32m'
+NONE='\033[0m'
+
+echo -e "${GREEN}"
+figlet "Spicetify"
+echo -e "${NONE}"
+
 sudo pacman -Sy spotify-launcher --noconfirm
 yay -S spicetify-cli --noconfirm
 
