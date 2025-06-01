8c7a684a0dd1124a828fa305e34b2a8e3c2ed8db
Fabian Liske <19373177+FabianLiske@users.noreply.github.com>
2025-06-01 22:39:18 +0200

    Removed unnecessary dir creations

--- scripts/deploy_utils.sh
+++ scripts/deploy_utils.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-mkdir -p "$HOME/.config/gtk-3.0"
-mkdir -p "$HOME/.local/share/fonts"
-mkdir -p "$HOME/.icons"
+mkdir -p "$HOME/.config"
 
 cp -r "$HOME/dotfiles/configs/monitoring/btop" "$HOME/.config/"
 cp -r "$HOME/dotfiles/configs/monitoring/fastfetch" "$HOME/.config/"
