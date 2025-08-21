212bcb3107e268655ea9a49f9a5c92373145ff6f
Fabian Liske <19373177+FabianLiske@users.noreply.github.com>
2025-08-22 01:27:48 +0200

    Initial Script for Yubikey

---
+++ scripts/misc/yubikey.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#set -euo pipefail
+
+# Colors
+GREEN='\033[0;32m'
+NONE='\033[0m'
+
+# Banner
+echo -e "${GREEN}"
+figlet "Yubikey"
+echo -e "${NONE}"
+
+sudo pacman -Sy \
+    yubikey-manager \
+    libfido2 \
+    --noconfirm
+
+#sudo mkdir -p "/etc/chromium/policies/managed/"
+
+#sudo cp "$HOME/dotfiles/configs/apps/chromium/extensions.json" "/etc/chromium/policies/managed/extensions.json"
+#sudo cp "$HOME/dotfiles/configs/apps/chromium/preferences.json" "/etc/chromium/policies/managed/preferences.json"
